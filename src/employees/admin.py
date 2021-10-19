57b4e8cfa46032642770625d66844f00fa9443cd
BondarenkoAlexandr <sanyasashkabond32@gmail.com>
2021-10-19 17:31:59 +0300

    added forms fo admin panel

--- src/employees/admin.py
+++ src/employees/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
-from employees.models import Employee
+from employees.models import Employee, Review, SEOEmployee
 
-admin.site.register(Employee)
+
+class EmployeeInlines(admin.StackedInline):
+    model = Review
+
+
+class SEOEmployeeInlines(admin.StackedInline):
+    model = SEOEmployee
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    inlines = (EmployeeInlines, SEOEmployeeInlines)
+    search_fields = ['name']
+
+
+admin.site.register(Employee, EmployeeAdmin)
