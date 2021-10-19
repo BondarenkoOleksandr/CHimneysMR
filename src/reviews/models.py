23c454feb813de90538d44245d49b32ef0568250
BondarenkoAlexandr <sanyasashkabond32@gmail.com>
2021-10-19 17:04:16 +0300

    changed review model

--- src/reviews/models.py
+++ src/reviews/models.py
@@ -5,13 +5,13 @@ from django.db import models
 from locations.models import State, City
 
 
-# class Review(models.Model):
-#     name = models.CharField(max_length=250)
-#     logo = models.FileField(upload_to='reviews/')
-#     city = models.ForeignKey(to=City, on_delete=models.SET_NULL, null=True)
-#     state = models.ForeignKey(to=State, on_delete=models.SET_NULL, null=True)
-#     text = models.TextField()
-#     date = models.DateField(auto_now_add=True)
-#
-#     def __str__(self):
-#         return self.name
+class Review(models.Model):
+    name = models.CharField(max_length=250)
+    logo = models.FileField(upload_to='reviews/')
+    city = models.ForeignKey(to=City, on_delete=models.SET_NULL, null=True)
+    state = models.ForeignKey(to=State, on_delete=models.SET_NULL, null=True)
+    text = models.TextField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
