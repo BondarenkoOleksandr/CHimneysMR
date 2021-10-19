fc87d0e97468083a899f375d1bdfab48757a40be
BondarenkoAlexandr <sanyasashkabond32@gmail.com>
2021-10-19 18:10:34 +0300

    refactoring all serializers and view

--- src/services/admin.py
+++ src/services/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
-from services.models import Service, ServiceCategory
+from nested_inline.admin import NestedStackedInline
 
-admin.site.register(Service)
+from services.models import Service, ServiceCategory, ServiceArticle, ServiceReview, SEOServiceArticle
+
+
+class ServiceReviewInline(admin.StackedInline):
+    model = ServiceReview
+
+
+class SEOServiceInlines(admin.StackedInline):
+    model = SEOServiceArticle
+
+
+class ServiceArticleInlines(admin.StackedInline):
+    model = ServiceArticle
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    inlines = (ServiceArticleInlines, ServiceReviewInline)
+    search_fields = ['category__name', 'name']
+
+
+admin.site.register(Service, ServiceAdmin)
 admin.site.register(ServiceCategory)
